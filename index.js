75f00e421ee57bdaa3f179c3dcb5dcfd564c33f0
nihalmohd <nihalmohd828@gmail.com>
2023-11-08 10:57:57 +0530

    basics is over

---
+++ index.js
@@ -0,0 +1,14 @@
+const express  = require("express")
+const dotenv = require("dotenv")
+const app = express()
+
+dotenv.config()
+app.use(express.json());
+
+
+
+
+const PORT = process.env.PORT
+app.listen(PORT,()=>{
+    console.log("Server connected successfully on port " ,PORT);
+})
